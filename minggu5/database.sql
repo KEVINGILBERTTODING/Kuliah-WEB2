7ea4d97546adbd1a9f378af23fab1b5a4c39cba3
KEVINGILBERTTODING <122202002832@mhs.dinus.ac.id>
2021-04-02 07:53:58 +0700

    latihan minggu 5

---
+++ minggu5/database.sql
@@ -0,0 +1,138 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 01, 2021 at 11:47 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `rental_mobil`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kendaraan`
+--
+
+CREATE TABLE `kendaraan` (
+  `no_plat` char(10) NOT NULL,
+  `tahun` int(4) DEFAULT NULL,
+  `tarif` int(11) DEFAULT NULL,
+  `status` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pelanggan`
+--
+
+CREATE TABLE `pelanggan` (
+  `no_ktp` varchar(20) NOT NULL,
+  `nama` varchar(30) DEFAULT NULL,
+  `alamat` varchar(30) DEFAULT NULL,
+  `telepon` varchar(13) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sopir`
+--
+
+CREATE TABLE `sopir` (
+  `id_sopir` char(10) NOT NULL,
+  `nama` varchar(30) DEFAULT NULL,
+  `alamat` varchar(30) DEFAULT NULL,
+  `telepon` varchar(13) DEFAULT NULL,
+  `sim` varchar(20) DEFAULT NULL,
+  `tarif` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tipe_kendaraan`
+--
+
+CREATE TABLE `tipe_kendaraan` (
+  `id_type` char(10) NOT NULL,
+  `type` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `no_transaksi` varchar(5) NOT NULL,
+  `tanggal_pesan` date DEFAULT NULL,
+  `tanggal_pinjam` date DEFAULT NULL,
+  `jam_kembali_rencana` time DEFAULT NULL,
+  `tanggal_kembali_realisasi` date DEFAULT NULL,
+  `jam_kembali_realisasi` time DEFAULT NULL,
+  `denda` int(10) DEFAULT NULL,
+  `kilometer_pinjam` int(20) DEFAULT NULL,
+  `kilometer_kembali` int(20) DEFAULT NULL,
+  `bbm_pinjam` varchar(15) DEFAULT NULL,
+  `bbm_kembali` varchar(15) DEFAULT NULL,
+  `kondisi_mobil_pinjam` varchar(50) DEFAULT NULL,
+  `kondisi_mobil_kembali` varchar(50) DEFAULT NULL,
+  `kerusakan` varchar(50) DEFAULT NULL,
+  `biaya_kerusakan` int(10) DEFAULT NULL,
+  `biaya_bbm` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `kendaraan`
+--
+ALTER TABLE `kendaraan`
+  ADD PRIMARY KEY (`no_plat`);
+
+--
+-- Indexes for table `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  ADD PRIMARY KEY (`no_ktp`);
+
+--
+-- Indexes for table `sopir`
+--
+ALTER TABLE `sopir`
+  ADD PRIMARY KEY (`id_sopir`);
+
+--
+-- Indexes for table `tipe_kendaraan`
+--
+ALTER TABLE `tipe_kendaraan`
+  ADD PRIMARY KEY (`id_type`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`no_transaksi`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
