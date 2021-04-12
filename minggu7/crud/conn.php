7cd2aead6b4aeb4deacdd5b8a79c22958f8cf633
KEVINGILBERTTODING <122202002832@mhs.dinus.ac.id>
2021-04-12 10:39:42 +0800

    latihan dan praktikum minggu 7

---
+++ minggu7/crud/conn.php
@@ -0,0 +1,10 @@
+<?php
+$engi = "mysql";
+$host = "localhost";
+$dbse = "crud_pdo";
+$user = "root";
+$pass = "";
+$koneksi = new PDO(
+    $engi . ':dbname=' . $dbse . ";host=" . $host,
+    $user,
+    $pass);
