438782b197d2e53b6cf0ffcd5a7f80d74bf98051
KEVINGILBERTTODING <122202002832@mhs.dinus.ac.id>
2021-06-16 23:38:14 +0800

    latihan dan praktikum minggu11

---
+++ minggu11/praktikum/bcit-ci-CodeIgniter-b73eb19/application/config/routes.php
@@ -0,0 +1,8 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+$route['default_controller'] = 'welcome';
+$route['about'] = 'welcome/about';
+$route['about'] = 'welcome/contact';
+$route['404_override'] = '';
+$route['translate_uri_dashes'] = FALSE;
---
+++ minggu11/praktikum/bcit-ci-CodeIgniter-b73eb19/application/controllers/Welcome.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Welcome extends CI_Controller
+{
+
+	public function index()
+	{
+		$this->load->view('welcome_message');
+	}
+
+	public function about()
+	{
+		$this->load->view('about');
+	}
+
+	public function contact()
+	{
+		$this->load->view('contact');
+	}
+}
